005f2dd3358762bd967e1caa1d561c22c97b9ecf
cesfierro <cesarafierro@outlook.com>
2024-05-16 11:58:38 -0700

    Finished creating ship object, movement, and fullscreen settings.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
     """Overall class to manage game assets and behaviour"""
 
@@ -9,25 +11,54 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
       
         
-
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT():
-                    sys.exit()
-                    
-            self.screen.fill(self.settings.bg_color)
-            # Show the most recently drawn screen
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(60)
-        
+
+    def _check_events(self):
+        # Watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    self._check_keydown_events(event)
+                elif event.type == pygame.KEYUP:
+                    self._check_keyup_events(event)
+                    
+
+    def _check_keydown_events(self, event):
+        # Respond to keypresses
+        if event.key == pygame.K_w:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_s:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
+            
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_w:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_s:
+            self.ship.moving_down = False
+
+    def _update_screen(self):
+        # Update images on screen, flip to new screen
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()   
+        pygame.display.flip()
+
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     ai = AlienInvasion()
---
+++ ship.py
@@ -0,0 +1,38 @@
+import pygame
+
+class Ship:
+    """Class to manage the ship"""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and starting position"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship and create rect
+        self.image = pygame.image.load('Ship.bmp')
+        self.rect = self.image.get_rect()
+
+        # Start the ship at the mid left of the screen
+        self.rect.midleft = self.screen_rect.midleft
+
+        # Store a float for the ship's exact veritcal position\
+        self.y = float(self.rect.y)
+
+        # Movement flag
+        self.moving_up = False
+        self.moving_down = False
+    
+    def update(self):
+        # Update the ship's position based on flags
+        # Update the 'y' value, not the rect
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.ship_speed
+        # Update the rect object for self 'y'
+        self.rect.y = self.y
+
+    def blitme(self):
+        """Draw the ship at its current location"""
+        self.screen.blit(self.image, self.rect)
